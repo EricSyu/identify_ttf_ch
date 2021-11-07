2c10654e69db3546b41bd9256bd5c145c58f2f1b
WeiJhe <sky820902@gmail.com>
2021-11-08 02:29:07 +0800

    add TtfSortor to classify TTF files

--- identify_ttf.py
+++ identify_ttf.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess
 import hanzidentifier
+import os
+import shutil
 
 class Font:
     def __init__(self, unicode:str):
@@ -25,7 +27,7 @@ class TtfFile:
         self.path = path
         self.fonts = []
 
-    def analysis(self):
+    def parse(self):
         comp_proc = subprocess.run(['otfinfo', '-u', self.path], capture_output = True)
         output = comp_proc.stdout.decode('utf-8')
         lines = output.split('\n')
@@ -49,10 +51,44 @@ class TtfFile:
     def __str__(self):
         return f'{self.path}\t{self.get_traditional_cnt()}\t{self.get_simplified_cnt()}'
 
+    def move(self, destination:str):
+        shutil.move(self.path, destination)
+
+class TtfSortor:
+    def __init__(self, dir:str):
+        self.dir = os.path.abspath(dir)
+        self.ttf_files = []
+    
+    def get_all_ttf_files(self):
+        for file_path in os.listdir(self.dir):
+            if file_path.upper().endswith('.TTF'):
+                p = os.path.abspath(self.dir + '/' + file_path)
+                f = TtfFile(p)
+                f.parse()
+                self.ttf_files.append(f)
+        return self.ttf_files
+
+    def classify(self):
+        traditional_dir = self.dir + '/traditional'
+        simplified_dir = self.dir + '/simplified'
+        if not os.path.exists(traditional_dir):
+            os.mkdir(traditional_dir)
+        if not os.path.exists(simplified_dir):
+            os.mkdir(simplified_dir)
+        
+        for f in self.ttf_files:
+            if f.get_traditional_cnt() > 9000:
+                f.move(traditional_dir)
+            else:
+                f.move(simplified_dir)
+            
+
 if __name__ == '__main__':
-    file_path = sys.argv[1]
-    print(file_path)
-    t = TtfFile(file_path)
-    t.analysis()
-    print('File\t繁體\t簡體')
-    print(t)
+    if len(sys.argv) == 1:
+        print('Please input a directory includes TTF files.')
+        exit()
+
+    dir_path = sys.argv[1]
+    sorter = TtfSortor(dir_path)
+    sorter.get_all_ttf_files()
+    sorter.classify()
